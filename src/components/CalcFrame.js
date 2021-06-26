3946e7bd7b9bf8f879a0d6dc07f68548907120ab
danjhkim <danjhkim@gmail.com>
2021-06-25 23:38:38 -0400

    fixed scaling numbers

--- src/components/CalcFrame.js
+++ src/components/CalcFrame.js
@@ -1,42 +1,61 @@
-import React, { useState, useEffect, useRef, useContext } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import Buttons from './Buttons';
 
-import { OperandContext } from "../contexts/OperandContext"
-
+import { OperandContext } from '../contexts/OperandContext';
 
 const CalcFrame = () => {
-    const opcontext = useContext(OperandContext)
+	const opcontext = useContext(OperandContext);
 
-    const [scale, setScale] = useState(1)
-    const node = useRef()
+	const [scale, setScale] = useState(1);
+	const node = useRef();
 
-    useEffect(() => {
-        const parentNode = node.current.parentNode
-        const availableWidth = parentNode.offsetWidth
-        const actualWidth = node.current.offsetWidth
-        const actualScale = availableWidth / actualWidth
+	useEffect(() => {
+		window.addEventListener('resize', function () {
+			const parentNode = node.current.parentNode;
+			const availableWidth = parentNode.offsetWidth;
+			const actualWidth = node.current.offsetWidth;
+			const actualScale = availableWidth / actualWidth;
+			if (actualScale < 1) {
+				setScale(actualScale);
+			} else if (scale < 1) {
+				setScale(1);
+			}
+		});
+	}, [scale]);
 
-        if (scale === actualScale)
-            return
+	useEffect(() => {
+		const parentNode = node.current.parentNode;
+		const availableWidth = parentNode.offsetWidth;
+		const actualWidth = node.current.offsetWidth;
+		const actualScale = availableWidth / actualWidth;
 
-        if (actualScale < 1) {
-            setScale(actualScale)
-        } else if (scale < 1) {
-            setScale(1)
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [opcontext.result, opcontext.answer])
+		if (scale === actualScale) return;
 
-    return (
-        <div className="calcbody">
-            <div className="calcscreen">
-                <div className="openrand">{opcontext.opScreen}</div>
-                <div className="result"><div className="autoscale" style={{ transform: `scale(${scale},${scale})` }} ref={node}>{opcontext.answer ? opcontext.answer : opcontext.result}</div></div>
-                {/* this solves the issue of saving the answer and having to the result refresh nice */}
-            </div>
-            <Buttons />
-        </div >
-    );
-}
+		if (actualScale < 1) {
+			setScale(actualScale);
+		} else if (scale < 1) {
+			setScale(1);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [opcontext.result, opcontext.answer]);
+
+	return (
+		<div className='calcbody'>
+			<div className='calcscreen'>
+				<div className='openrand'>{opcontext.opScreen}</div>
+				<div className='result'>
+					<div
+						className='autoscale'
+						style={{ transform: `scale(${scale},${scale})` }}
+						ref={node}>
+						{opcontext.answer ? opcontext.answer : opcontext.result}
+					</div>
+				</div>
+				{/* this solves the issue of saving the answer and having to the result refresh nice */}
+			</div>
+			<Buttons />
+		</div>
+	);
+};
 
 export default CalcFrame;
